02da732312b5afb9ba7c6da4957f28caf6a4c288
gordon <wgordonw1@gmail.com>
2013-12-22 14:13:51 -0500

    start over with a basic widget that splits the phone number into the country code, national number, and extension

--- phonenumber_field/widgets.py
+++ phonenumber_field/widgets.py
@@ -1,51 +1,28 @@
 #-*- coding: utf-8 -*-
-
-from babel import Locale
-
-from phonenumbers.data import _COUNTRY_CODE_TO_REGION_CODE
-
-from django.utils import translation
-from django.forms import Select, TextInput
+from django.forms import TextInput
 from django.forms.widgets import MultiWidget
+from phonenumber_field.phonenumber import PhoneNumber, to_python
 
-from phonenumber_field.phonenumber import to_python
-
-
-class PhonePrefixSelect(Select):
-
-    initial = None
-
-    def __init__(self, initial=None):
-        choices = [('', '---------')]
-        locale = Locale(translation.get_language())
-        for prefix, values in _COUNTRY_CODE_TO_REGION_CODE.iteritems():
-            prefix = '+%d' % prefix
-            if initial and initial in values:
-                self.initial = prefix
-            for country_code in values:
-                country_name = locale.territories.get(country_code)
-                if country_name:
-                    choices.append((prefix, u'%s %s' % (country_name, prefix)))
-        return super(PhonePrefixSelect, self).__init__(choices=sorted(choices, key=lambda item: item[1]))
-
-    def render(self, name, value, *args, **kwargs):
-        return super(PhonePrefixSelect, self).render(name, value or self.initial, *args, **kwargs)
-
-class PhoneNumberPrefixWidget(MultiWidget):
+class PhoneNumberWidget(MultiWidget):
     """
     A Widget that splits phone number input into:
-    - a country select box for phone prefix
+    - an input for the country code prefix
     - an input for local phone number
+    - an input for extension
     """
     def __init__(self, attrs=None, initial=None):
-        widgets = (PhonePrefixSelect(initial),TextInput(),)
-        super(PhoneNumberPrefixWidget, self).__init__(widgets, attrs)
+        widgets = (TextInput(),TextInput(),TextInput())
+        super(PhoneNumberWidget, self).__init__(widgets, attrs)
 
     def decompress(self, value):
-        if value:
-            return value.split('.')
-        return [None, None]
+        if not isinstance(value, PhoneNumber):
+            value = to_python(value)
+        return [value.country_code, value.national_number, value.extension]
 
     def value_from_datadict(self, data, files, name):
-        values = super(PhoneNumberPrefixWidget, self).value_from_datadict(data, files, name)
-        return '%s.%s' % tuple(values)
+        country_code, national_number, extension = super(PhoneNumberWidget, self).value_from_datadict(data, files, name)
+        if country_code:
+            country_code = "+%s" % country_code
+        if extension:
+            extension = "x%s" % extension
+        return '%s%s%s' % (country_code, national_number, extension)
