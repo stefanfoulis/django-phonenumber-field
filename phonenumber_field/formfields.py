e9947e7a41a1c23213ad71978c8255e17b917f0c
gordon <wgordonw1@gmail.com>
2014-03-08 18:33:18 -0500

    output the invalid input in the invalid error message

--- phonenumber_field/formfields.py
+++ phonenumber_field/formfields.py
@@ -1,5 +1,5 @@
 #-*- coding: utf-8 -*-
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import ugettext_lazy as _, string_concat
 from django.forms.fields import CharField
 from django.core.exceptions import ValidationError
 from phonenumber_field.validators import validate_international_phonenumber
@@ -17,5 +17,9 @@ class PhoneNumberField(CharField):
     def to_python(self, value):
         phone_number = to_python(value)
         if phone_number and not phone_number.is_valid():
-            raise ValidationError(self.error_messages['invalid'])
+            msg = string_concat(
+                self.error_messages['invalid'],
+                u" The provided value, {0}, is invalid.".format(phone_number.raw_input)
+            )
+            raise ValidationError(msg)
         return phone_number
