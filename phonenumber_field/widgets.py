fd183cca9a8665218292adbe714f5c4682828d2b
James Robert <github@jiaaro.com>
2015-01-19 20:59:32 -0500

    fix issue with language parsing
    Language codes returned by `translation.get_language()` such as `"en-us"` were failingl because the Local constructor expects `Locale("en", territory="US")`

--- phonenumber_field/widgets.py
+++ phonenumber_field/widgets.py
@@ -17,7 +17,7 @@ class PhonePrefixSelect(Select):
 
     def __init__(self, initial=None):
         choices = [('', '---------')]
-        locale = Locale(translation.get_language())
+        locale = Locale.parse(translation.get_language(), sep='-') 
         for prefix, values in _COUNTRY_CODE_TO_REGION_CODE.iteritems():
             prefix = '+%d' % prefix
             if initial and initial in values:
