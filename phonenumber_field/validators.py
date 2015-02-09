7d483eb0634909ddffdea634cd126e49537a785a
Logan Gunthorpe <logang@deltatee.com>
2015-02-09 14:52:52 -0700

    Support python 3.0 by revoming the u literal

--- phonenumber_field/validators.py
+++ phonenumber_field/validators.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+from __future__ import unicode_literals
+
 from django.core.exceptions import ValidationError
 from django.utils.translation import ugettext_lazy as _
 from phonenumber_field.phonenumber import to_python
@@ -8,4 +10,4 @@ from phonenumber_field.phonenumber import to_python
 def validate_international_phonenumber(value):
     phone_number = to_python(value)
     if phone_number and not phone_number.is_valid():
-        raise ValidationError(_(u'The phone number entered is not valid.'))
+        raise ValidationError(_('The phone number entered is not valid.'))
