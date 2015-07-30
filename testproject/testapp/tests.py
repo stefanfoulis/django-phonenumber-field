029869b48bb04bd4f8b58b3a29a42ab62db23fde
gordon <wgordonw1@gmail.com>
2015-07-30 17:58:11 -0400

    verify extension survives save

--- testproject/testapp/tests.py
+++ testproject/testapp/tests.py
@@ -39,6 +39,13 @@ class PhonenumerFieldAppTest(TestCase):
         
         tm = TestModel.objects.get(pk=pk)
         self.assertEqual(tm.phone.country_id, "CH")
+        
+        tm.phone.extension = "777"
+        tm.save()
+        
+        tm = TestModel.objects.get(pk=pk)
+        self.assertEqual(str(tm.phone.extension), "777")
+        self.assertEqual(str(tm.phone), '+41524242424x777')
     
     def test_save_blank_phone_to_database(self):
         from testapp.models import TestModelBlankPhone
