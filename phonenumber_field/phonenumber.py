190f184362f7ba273f0aab6cef814912abcbdd53
gordon <wgordonw1@gmail.com>
2015-07-31 12:20:39 -0400

    move region_code parsing from the to_python function to a method on the PhoneNumber class

--- phonenumber_field/phonenumber.py
+++ phonenumber_field/phonenumber.py
@@ -30,16 +30,57 @@ class PhoneNumber(phonenumbers.phonenumber.PhoneNumber):
         'NATIONAL': phonenumbers.PhoneNumberFormat.NATIONAL,
         'RFC3966': phonenumbers.PhoneNumberFormat.RFC3966,
     }
-
+    
+    def __init__(self, **kwargs):
+        raw = kwargs.get("raw_input", None)
+        if raw:
+            region_code, phone_number = self.parse_region_code(raw)
+            self.region_code = region_code
+            kwargs["raw_input"] = phone_number
+        
+        super(PhoneNumber, self).__init__(**kwargs)
+    
     @classmethod
     def from_string(cls, phone_number, region=None):
+        if not isinstance(phone_number, string_types):
+            raise TypeError("Supplied phone number was not a string")
+        
+        if not (isinstance(region, string_types) or region is None):
+            raise TypeError("Supplied region was not a string or None")
+        
         phone_number_obj = cls()
+        
+        region_code, phone_number = cls.parse_region_code(phone_number)
+        
         if region is None:
-            region = (getattr(settings, 'PHONENUMBER_DEFAULT_REGION', None)
-                      or getattr(settings, 'PHONENUMER_DEFAULT_REGION', None))
+            if region_code:
+                region = region_code
+            else:
+                region = (getattr(settings, 'PHONENUMBER_DEFAULT_REGION', None)
+                          or getattr(settings, 'PHONENUMER_DEFAULT_REGION', None))
         phonenumbers.parse(number=phone_number, region=region,
                            keep_raw_input=True, numobj=phone_number_obj)
+        
+        phone_number_obj.region_code = region
+        
         return phone_number_obj
+    
+    @classmethod
+    def parse_region_code(cls, value):
+        if not isinstance(value, string_types):
+            raise TypeError("Supplied value was not a string")
+        
+        result = value.split(cls.region_code_sep, 1)
+        len_result = len(result)
+        
+        if len_result == 1:
+            region_code, phone_number = (None, result[0])
+        elif len_result == 2:
+            region_code, phone_number = result
+        else:
+            region_code, phone_number = (None, "")
+        
+        return region_code, phone_number
 
     def __unicode__(self):
         format_string = getattr(settings, 'PHONENUMBER_DEFAULT_FORMAT', 'E164')
@@ -84,29 +125,19 @@ def to_python(value):
     if value in validators.EMPTY_VALUES:  # None or ''
         phone_number = None
     elif value and isinstance(value, string_types):
-        result = value.split(PhoneNumber.region_code_sep, 1)
-        len_result = len(result)
-        
-        if len_result == 1:
-            region_code, phone_number_str = (None, result[0])
-        elif len_result == 2:
-            region_code, phone_number_str = result
-        else:
-            region_code, phone_number_str = (None, "")
-        
         try:
-            phone_number = PhoneNumber.from_string(phone_number=phone_number_str)
+            phone_number = PhoneNumber.from_string(phone_number=value)
         except NumberParseException:
             # the string provided is not a valid PhoneNumber.
-            phone_number = PhoneNumber(raw_input=phone_number_str)
-        phone_number.region_code = region_code
-        
-    elif isinstance(value, phonenumbers.phonenumber.PhoneNumber) and not isinstance(value, PhoneNumber):
+            phone_number = PhoneNumber(raw_input=value)
+    elif (isinstance(value, phonenumbers.phonenumber.PhoneNumber) and
+          not isinstance(value, PhoneNumber)):
         phone_number = PhoneNumber(value)
     elif isinstance(value, PhoneNumber):
         phone_number = value
     else:
-        # TODO: this should somehow show that it has invalid data, but not completely die for
-        #       bad data in the database. (Same for the NumberParseException above)
+        # TODO: this should somehow show that it has invalid data, but not
+        #       completely die for bad data in the database.
+        #       (Same for the NumberParseException above)
         phone_number = None
     return phone_number
