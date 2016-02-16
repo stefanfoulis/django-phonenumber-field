74b7074f96f38274b325fba0de8a5aa1081a7b80
Davit Vardanyan <davoimail@gmail.com>
2016-02-16 16:39:57 +0400

    Removed unnecessary dot which causes to fail when entering blank number

--- phonenumber_field/widgets.py
+++ phonenumber_field/widgets.py
@@ -56,4 +56,4 @@ class PhoneNumberPrefixWidget(MultiWidget):
     def value_from_datadict(self, data, files, name):
         values = super(PhoneNumberPrefixWidget, self).value_from_datadict(
             data, files, name)
-        return '%s.%s' % tuple(values)
+        return '%s%s' % tuple(values)
