b5d6cf2487161507e63bce0030eaf3a10d97dcdc
themarcq <m.matys@ilo.pl>
2016-08-17 12:29:42 +0200

    fixed required argument

--- phonenumber_field/widgets.py
+++ phonenumber_field/widgets.py
@@ -59,7 +59,10 @@ class PhoneNumberPrefixWidget(MultiWidget):
     def value_from_datadict(self, data, files, name):
         values = super(PhoneNumberPrefixWidget, self).value_from_datadict(
             data, files, name)
-        return '%s.%s' % tuple(values)
+        if all(values):
+            return '%s.%s' % tuple(values)
+        return ''
+
 
 
 class PhoneNumberInternationalFallbackWidget(TextInput):
