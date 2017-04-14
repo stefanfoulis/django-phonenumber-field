b027752e1d66d45bdf8515d01ed9282bb5800594
Stefan Foulis <stefan@foulis.ch>
2017-04-15 01:07:05 +0200

    Testproject urls.py compatible with Django>=1.11

--- testproject/testproject/urls.py
+++ testproject/testproject/urls.py
@@ -1,17 +1,8 @@
-from django.conf.urls import patterns, include, url
+import django
+GTE_DJANGO_1_8 = django.VERSION >= (1, 8)
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
-
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'testproject.views.home', name='home'),
-    # url(r'^testproject/', include('testproject.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
-)
+if GTE_DJANGO_1_8:
+    urlpatterns = []
+else:
+    from django.conf.urls import patterns
+    urlpatterns = patterns('')
