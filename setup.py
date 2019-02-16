2ac17b322ddeb8990dc2a2bebfe42c28ec001789
Jon Dufresne <jon.dufresne@gmail.com>
2019-02-16 15:45:56 -0800

    Add trove classifiers for all supported Django & Python versions
    Per tox.ini, Django 1.11, 2.0, and 2.1 are supported along with Python
    3.4.

--- setup.py
+++ setup.py
@@ -29,6 +29,9 @@ setup(
     classifiers=[
         'Development Status :: 4 - Beta',
         'Framework :: Django',
+        'Framework :: Django :: 1.11',
+        'Framework :: Django :: 2.0',
+        'Framework :: Django :: 2.1',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
@@ -36,6 +39,7 @@ setup(
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
