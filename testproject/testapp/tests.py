484c30d88b3b623000d41095b3acbf6ab08edbc7
François Freitag <mail@franek.fr>
2019-02-21 10:49:06 +0100

    Remove django test instructions and unused testapp views

--- testproject/testapp/tests.py
+++ testproject/testapp/tests.py
@@ -1,10 +1,3 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.db.models import Q
 from django.test import TestCase
 
