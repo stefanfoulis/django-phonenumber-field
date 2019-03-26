stefanfoulis/django-phonenumber-field

commit fe946db5bb59740150cd7e1b05c1e39ee7f291bb

Inherit from Django CharField
Simplify initialization, avoid copying some CharField methods over and
let Django handle database compatibility regarding storage of null
values.

phonenumber_field/modelfields.py
@@ -2,8 +2,8 @@
 from __future__ import unicode_literals
 
 from django.conf import settings
-from django.core import checks, validators
-from django.db import connection, models
+from django.core import checks
+from django.db import models
 from django.utils.encoding import force_text
 from django.utils.translation import ugettext_lazy as _
 
@@ -40,7 +40,7 @@ class PhoneNumberDescriptor(object):
         instance.__dict__[self.field.name] = to_python(value, region=self.field.region)
 
 
-class PhoneNumberField(models.Field):
+class PhoneNumberField(models.CharField):
     attr_class = PhoneNumber
     descriptor_class = PhoneNumberDescriptor
     default_validators = [validate_international_phonenumber]
@@ -51,7 +51,6 @@ class PhoneNumberField(models.Field):
         kwargs.setdefault("max_length", 128)
         self.region = kwargs.pop("region", None)
         super(PhoneNumberField, self).__init__(*args, **kwargs)
-        self.validators.append(validators.MaxLengthValidator(self.max_length))
 
     def check(self, **kwargs):
         errors = super(PhoneNumberField, self).check(**kwargs)
@@ -64,9 +63,6 @@ class PhoneNumberField(models.Field):
         except ValueError as e:
             return [checks.Error(force_text(e), obj=self)]
         return []
-
-    def get_internal_type(self):
-        return "CharField"
 
     def get_prep_value(self, value):
         """
@@ -98,9 +94,4 @@ class PhoneNumberField(models.Field):
             "region": self.region,
             "error_messages": self.error_messages,
         }
-
-        if self.null and not connection.features.interprets_empty_strings_as_nulls:
-            defaults["empty_value"] = None
-
-        defaults.update(kwargs)
         return super(PhoneNumberField, self).formfield(**defaults)

commit b552acc538b9ffe4359b849d346307f1c5396186

Allow model field subclasses to override formfield
Regression introduced in fe946db5bb59740150cd7e1b05c1e39ee7f291bb.

phonenumber_field/modelfields.py
@@ -94,4 +94,5 @@ class PhoneNumberField(models.CharField):
             "region": self.region,
             "error_messages": self.error_messages,
         }
+        defaults.update(kwargs)
         return super(PhoneNumberField, self).formfield(**defaults)

tests/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 
+from phonenumber_field.formfields import PhoneNumberField
+
 from .models import NullablePhoneNumber
 
 
@@ -7,3 +9,7 @@ class PhoneNumberForm(forms.ModelForm):
     class Meta:
         model = NullablePhoneNumber
         fields = ["phone_number"]
+
+
+class CustomPhoneNumberFormField(PhoneNumberField):
+    pass

tests/models.py
@@ -15,6 +15,19 @@ class NullablePhoneNumber(models.Model):
     phone_number = PhoneNumberField(blank=True, null=True)
 
 
+class CustomPhoneNumberModelField(PhoneNumberField):
+    def formfield(self, **kwargs):
+        from .forms import CustomPhoneNumberFormField
+
+        return super(CustomPhoneNumberModelField, self).formfield(
+            form_class=CustomPhoneNumberFormField
+        )
+
+
+class CustomPhoneNumber(models.Model):
+    phone_number = CustomPhoneNumberModelField()
+
+
 class TestModel(models.Model):
     """Basic Field Test"""
 

tests/tests.py
@@ -15,7 +15,7 @@ from phonenumber_field.phonenumber import PhoneNumber, to_python
 from phonenumber_field.widgets import PhoneNumberInternationalFallbackWidget
 
 from . import models
-from .forms import PhoneNumberForm
+from .forms import CustomPhoneNumberFormField, PhoneNumberForm
 
 
 def phone_transform(obj):
@@ -413,3 +413,8 @@ class RegionPhoneNumberModelFieldTest(TestCase):
         self.assertIsInstance(error, checks.Error)
         self.assertTrue(error.msg.startswith("“invalid” is not a valid region code."))
         self.assertEqual(error.obj, InvalidRegionModel._meta.get_field("phone_field"))
+
+    def test_override_form_field(self):
+        phone_number = models.CustomPhoneNumber()
+        model_field = phone_number._meta.get_field("phone_number")
+        self.assertIsInstance(model_field.formfield(), CustomPhoneNumberFormField)