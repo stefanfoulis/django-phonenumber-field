cde2cbd4d60f4ea5b2cb17003508438b80d6224f
François Freitag <mail@franek.fr>
2019-05-28 17:07:49 +0200

    Drop support for Django 2.0
    Django 2.0 is not maintained since Apr 1st, 2019.
    https://www.djangoproject.com/download/#supported-versions

--- setup.py
+++ setup.py
@@ -28,7 +28,6 @@ setup(
         "Development Status :: 5 - Production/Stable",
         "Framework :: Django",
         "Framework :: Django :: 1.11",
-        "Framework :: Django :: 2.0",
         "Framework :: Django :: 2.1",
         "Framework :: Django :: 2.2",
         "Intended Audience :: Developers",
