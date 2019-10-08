5342eb2cabfc5f06fdd93a9741574029c37efd63
László Károlyi <laszlo@karolyi.hu>
2019-10-08 19:13:29 +0200

    Forgot to remove ValidationError

--- phonenumber_field/modelfields.py
+++ phonenumber_field/modelfields.py
@@ -1,6 +1,5 @@
 from django.conf import settings
 from django.core import checks
-from django.core.exceptions import ValidationError
 from django.db import models
 from django.utils.encoding import force_text
 from django.utils.translation import gettext_lazy as _
