11747bc1c557a6099ace9f2d2872b4ba63119cd9
Ahmed Shahwan <a.shahwan42@gmail.com>
2020-10-11 12:57:04 +0200

    return phone_number.as_e164 to be serializable
    fixes #225

--- phonenumber_field/serializerfields.py
+++ phonenumber_field/serializerfields.py
@@ -12,4 +12,4 @@ class PhoneNumberField(serializers.CharField):
         phone_number = to_python(data)
         if phone_number and not phone_number.is_valid():
             raise ValidationError(self.error_messages["invalid"])
-        return phone_number
+        return phone_number.as_e164
