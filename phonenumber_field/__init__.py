927af63b6b442c6850100d670c07a8e7351158d8
François Freitag <mail@franek.fr>
2021-01-21 13:25:03 +0100

    Use setuptools_scm to get the version number
    Follow recommendation from https://jazzband.co/about/releases#packaging.
    
    Files tracked with the version control system are included in the source
    distribution. That avoids the error-prone listing of files in
    MANIFEST.in.
    
    Don’t package the .github directory and sonar-project.properties, they
    contain CI configuration for the project and should not be distributed.
    
    Don’t package .gitignore, it is specific to version control and should
    not be distributed.
    
    Refs #224
    
    Reviewed-by: Stefan Foulis

--- phonenumber_field/__init__.py
+++ phonenumber_field/__init__.py
@@ -1,1 +1,7 @@
-__version__ = "5.0.0"
+try:
+    from .version import version
+except ImportError:
+    # The version module is written by setuptools_scm.
+    __version__ = None
+else:
+    __version__ = version
